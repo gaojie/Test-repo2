a68d6a257eb67656e28defa29c7c1b8a3eaf5b2e
Gaojie <gaojiengb@gmail.com>
2010-12-06 21:13:11 +0800

    Test 20

--- blkdev.h
+++ blkdev.h
@@ -4,7 +4,7 @@
 
  ***   This header was automatically generated from a Linux kernel heade r
  ***   of the same name, to make information necessary for userspace to
- ***   call into the kernel available to libc.  It contains only constants,
+ ***   call into the kernel available to libc.  It contains only const ants,
 
  ***   structures, and macros generated from the original header, and thus,
  ***   contains no copyrightable information.
