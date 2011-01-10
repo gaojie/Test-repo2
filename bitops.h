e28d126234df7fe302992d5d690bb9ef7aca8e9a
Gaojie <gaojiengb@gmail.com>
2011-01-10 12:57:17 +0800

    Test 100

--- bitops.h
+++ bitops.h
@@ -1,7 +1,7 @@
 /****************************************************************************
 
  ****************************************************************************
- ***
+ ******
  ***   This header was automatically generated from a Linux kernel header
  ***   of the same name, to make information necessary for userspace to
  ***   call into the kernel available to libc.  It contains only constants,
